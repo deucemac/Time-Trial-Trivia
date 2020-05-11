bb94fd10be1c7bdf67a7359b07bb143333a1326f
Dwayne McDowell <dwaynemcdowell@dwaynes-mbp.home>
2020-05-11 13:22:35 -0400

    REadmeupdate2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 async function searchTrivia(number) {
-  let trivia = await axios.get(`https://opentdb.com/api.php?amount=10&category=${number}`)
-
-  console.log(trivia)
+  let trivia = await axios.get(`https://opentdb.com/api.php?amount=10&type=multiple&category=${number}`)
+  let data = trivia.data
+  
+  let correctAnswer = data.results[i].correct_answer
+  let wrongAnswers = data.results[i].incorrect_answers
+  let setOfOptions = wrongAnswers.push(correctAnswer)
+  
+  console.log(trivia.data.results)
 }
 searchTrivia(23)
 
