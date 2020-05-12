a128b1f905b2f681853ca1bf7511f9ae6475d058
Dwayne McDowell <dwaynemcdowell@dwaynes-mbp.home>
2020-05-12 12:22:26 -0400

    cleared-page1

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// async function searchTrivia(number) {
-//   let trivia = await axios.get(`https://opentdb.com/api.php?amount=10&type=multiple&category=${number}`)
-//   let data = trivia.data
+async function searchTrivia(number) {
+  let trivia = await axios.get(`https://opentdb.com/api.php?amount=4&type=multiple&category=${number}`)
+  let data = trivia.data
   
-//   let correctAnswer = data.results[4].correct_answer
-//   console.log(correctAnswer)
+  let correctAnswer = data.results[4].correct_answer
+  console.log(correctAnswer)
   
-//   let wrongAnswer1 = data.results[4].incorrect_answers[0]
-//   let wrongAnswer2 = data.results[4].incorrect_answers[1]
-//   let wrongAnswer3 = data.results[4].incorrect_answers[2]   
-//   // console.log('3rd wrong answer ' + wrongAnswer3)
-//   let setOfOptions = [correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3]
-//   console.log(setOfOptions)
+  let wrongAnswer1 = data.results[4].incorrect_answers[0]
+  let wrongAnswer2 = data.results[4].incorrect_answers[1]
+  let wrongAnswer3 = data.results[4].incorrect_answers[2]   
+  // console.log('3rd wrong answer ' + wrongAnswer3)
+  let setOfOptions = [correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3]
+  console.log(setOfOptions)
 
-// }
+}
 // searchTrivia(23)
 
 // axios.get('https://opentdb.com/api.php?amount=10&category=23')
@@ -35,9 +35,19 @@ button.addEventListener('click', function(e) {
   e.preventDefault()
   let inputValue = input.value
   console.log(inputValue)
+  removeElements()
+  return inputValue
 })
 
+function removeElements() {
+  let allElements = document.querySelector('.view-page')
+  allElements.remove()
+  document.body.style.backgroundSize = 'cover';
+}
 
+function presentQuestions() {
+ 
+}
 
 
 
