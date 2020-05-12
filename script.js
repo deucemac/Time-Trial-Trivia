208321ff1bb5a49af0eff0868a7d76607c7ba173
Dwayne McDowell <dwaynemcdowell@dwaynes-mbp.home>
2020-05-12 11:56:50 -0400

    moreJS

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
-async function searchTrivia(number) {
-  let trivia = await axios.get(`https://opentdb.com/api.php?amount=10&type=multiple&category=${number}`)
-  let data = trivia.data
+// async function searchTrivia(number) {
+//   let trivia = await axios.get(`https://opentdb.com/api.php?amount=10&type=multiple&category=${number}`)
+//   let data = trivia.data
   
-  let correctAnswer = data.results[4].correct_answer
-  console.log(correctAnswer)
+//   let correctAnswer = data.results[4].correct_answer
+//   console.log(correctAnswer)
   
-  let wrongAnswer1 = data.results[4].incorrect_answers[0]
-  let wrongAnswer2 = data.results[4].incorrect_answers[1]
-  let wrongAnswer3 = data.results[4].incorrect_answers[2]   
-  // console.log('3rd wrong answer ' + wrongAnswer3)
-  let setOfOptions = [correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3]
-  console.log(setOfOptions)
-  // console.log(setOfOptions)
-  // let setOfOptions = wrongAnswers.push(correctAnswer)
-  // console.log(setOfOptions)
-}
-searchTrivia(23)
+//   let wrongAnswer1 = data.results[4].incorrect_answers[0]
+//   let wrongAnswer2 = data.results[4].incorrect_answers[1]
+//   let wrongAnswer3 = data.results[4].incorrect_answers[2]   
+//   // console.log('3rd wrong answer ' + wrongAnswer3)
+//   let setOfOptions = [correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3]
+//   console.log(setOfOptions)
+
+// }
+// searchTrivia(23)
 
 // axios.get('https://opentdb.com/api.php?amount=10&category=23')
 //   .then((res) => {
 //   console.log(res)
 //   })
+
+// function grabName(name) {
+//   name.preventDefault()
+//   const input = document.getElementById('fname')
+//   const value = input.value
+//   console.log(value)
+//   return value
+// }
+// const button = document.querySelector('.submit')
+
+const input = document.getElementById('fname')
+const button = document.querySelector('.submit')
+button.addEventListener('click', function(e) {
+  e.preventDefault()
+  let inputValue = input.value
+  console.log(inputValue)
+})
 
 
 
