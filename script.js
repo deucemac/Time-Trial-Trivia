533dffdd853b980e5e198d4c923aa45b1be69e7c
Dwayne McDowell <dwaynemcdowell@dwaynes-mbp.home>
2020-05-11 23:55:32 -0400

    css&html

--- script.js
+++ script.js
@@ -2,11 +2,18 @@ async function searchTrivia(number) {
   let trivia = await axios.get(`https://opentdb.com/api.php?amount=10&type=multiple&category=${number}`)
   let data = trivia.data
   
-  let correctAnswer = data.results[i].correct_answer
-  let wrongAnswers = data.results[i].incorrect_answers
-  let setOfOptions = wrongAnswers.push(correctAnswer)
+  let correctAnswer = data.results[4].correct_answer
+  console.log(correctAnswer)
   
-  console.log(trivia.data.results)
+  let wrongAnswer1 = data.results[4].incorrect_answers[0]
+  let wrongAnswer2 = data.results[4].incorrect_answers[1]
+  let wrongAnswer3 = data.results[4].incorrect_answers[2]   
+  // console.log('3rd wrong answer ' + wrongAnswer3)
+  let setOfOptions = [correctAnswer, wrongAnswer1, wrongAnswer2, wrongAnswer3]
+  console.log(setOfOptions)
+  // console.log(setOfOptions)
+  // let setOfOptions = wrongAnswers.push(correctAnswer)
+  // console.log(setOfOptions)
 }
 searchTrivia(23)
 
